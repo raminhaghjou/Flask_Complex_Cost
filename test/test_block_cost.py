bef23f6d60ded4cc87eb185733f3d237995e7b68
Ramin Haghjou <haghjouramin@gmail.com>
2023-06-24 16:36:16 +0330

    update test

--- test/test_block_cost.py
+++ test/test_block_cost.py
@@ -20,10 +20,15 @@ class TestBlock():
     #     assert 20000 in b.BlockCost().set_item(self.value[0])
         
     @pytest.mark.parametrize('index', [1,2,3,4,5])
-    def test_del_item(self, index):
+    def test_del_item(self, index, setup):
         with pytest.raises(ValueError) as exc:
-            b.__delitem__(index)
-        assert (exc.value == "Index out of range")
+            self.block.__delitem__(index)
+        assert (str(exc.value) == "Index out of range")
         
+    @pytest.mark.parametrize('value', [1,2,3,4,5])
     def test_set_item(self):
-        assert b.set_item(self.value)
+        assert self.block.set_item(value)
+        
+    @pytest.mark.parametrize('index, value', [1,2,3,4,5])
+    def test__setitem__for_if_value_less_than_zero(self):
+        assert self.block.__setitem__(self.index, self.value) != None
